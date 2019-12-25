33e0f426ecdecf3e9d38a3462eb3fea57b9f2056
cwinters8 <clwinters8@gmail.com>
2019-12-25 12:53:17 -0800

    add description, image, and layout

--- client/web/src/App.js
+++ client/web/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// images
+import spices from './Images/anise-aroma-art-bazaar-277253.jpg';
+
+class App extends React.Component {
+  render() { 
+    return (
+      <div className="App">
+        <header>
+          <h1>What's for Dinner?</h1>
+        </header>
+        <div className="grid">
+          <div>
+            <h2>Your meal planning and grocery list helper.</h2>
+            <p>"What's for Dinner?" is a web and mobile app designed to help make your food planning life easier.</p>
+            <p>Features:</p>
+            <ul>
+              <li>Meal Planner</li>
+              <li>Recipe Book</li>
+              <li>Grocery List Builder</li>
+            </ul>
+          </div>
+          <img className="spices" src={spices} alt="Lunch Table"/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
